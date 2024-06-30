2d60ee04900c05b6b3a24728a893a17ca6976e51
Riku <ree9.business@gmail.com>
2024-06-30 21:26:46 +0900

    [add]create method,delete methodの作成

--- backend/src/post/post.service.ts
+++ backend/src/post/post.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreatePostInput } from './dto/createPost.dto';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+  constructor(private readonly prismaservice: PrismaService) {}
+
+  async createPost(createPostInput: CreatePostInput) {
+    const { user_id, image_url, caption } = createPostInput;
+    return await this.prismaservice.post.create({
+      data: {
+        user_id,
+        image_url,
+        caption,
+      },
+    });
+  }
+
+  async deletePost(post_id: number) {
+    return await this.prismaservice.post.delete({
+      where: { post_id },
+    });
+  }
+}

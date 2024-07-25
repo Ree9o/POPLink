5bcf24282be3f0556617dabeb81a7a792d6fc563
Riku <ree9.business@gmail.com>
2024-07-25 15:52:27 +0900

    [add] getPosts,getAllPostsの作成

--- backend/src/post/post.service.ts
+++ backend/src/post/post.service.ts
@@ -1,10 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreatePostInput } from './dto/createPost.dto';
+import { Post } from '@prisma/client';
 
 @Injectable()
 export class PostService {
   constructor(private readonly prismaservice: PrismaService) {}
+
+  async getPosts(user_id: number): Promise<Post[]> {
+    return await this.prismaservice.post.findMany({
+      where: { user_id },
+    });
+  }
+
+  async getAllPosts(): Promise<Post[]> {
+    return await this.prismaservice.post.findMany();
+  }
 
   async createPost(createPostInput: CreatePostInput) {
     const { user_id, image_url, caption } = createPostInput;
